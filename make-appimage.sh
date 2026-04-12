25e1298d1f7f554d88e6eaf081a4d2bd7571a2d5
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:48:00 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/FWGS/xash3d-fwgs/refs/heads/master/game_launch/icon-xash-material.png
 export DEPLOY_OPENGL=1
