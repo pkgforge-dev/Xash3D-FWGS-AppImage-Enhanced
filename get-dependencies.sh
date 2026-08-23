8ce4449a38d8f476443c44b7173ab45472ebab07
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:35:13 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,13 +8,11 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     fontconfig \
-    libdecor   \
-    python     \
     sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building Xash3D-FWGS..."
 echo "---------------------------------------------------------------"
