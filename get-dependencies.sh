d39180fb20f090dea77a1da6e575c44a22e2d17c
Link4Electronics <link4electronics@gmail.com>
2026-09-10 22:34:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm fontconfig sdl2
+pacman -Syu --noconfirm fontconfig sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
